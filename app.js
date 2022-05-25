bb1fb9fe2050b02f92f1917a69db9d2080bc19f5
MatheusStefan <roccomatheus0@gmail.com>
2022-05-25 15:47:11 -0300

    Criando e salvando usuario

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 const http = require('http');
+const url = require('url');
+const queryString = require('query-string');
+// fs = file string, usado para salvar arquivos
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   
+  // ACOES QUE QUERO FAZER NESSE PROJETO
+
+  let resposta;
+
+  // criar usuario
+      // receber informacoes do usuario direto na barra de busca
+      const params = queryString.parse(url.parse(req.url, true).search);
+      
+      // salvar essas informacoes
+      fs.writeFile('users/' + params.id + '.txt', JSON.stringify(params), function (err){
+        if(err) throw err;
+        console.log('Saved!');
+      });
+
+      resposta = 'Usuario criado com sucesso!';
+  // atualizar usuario
+  // selecionar/pesquisar usuario
+  // remover usuario
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end(resposta);
 });
 
 server.listen(port, hostname, () => {
